5c91b815dafb34de338aa905b41c8982a643c577
yczero <yc4038@naver.com>
2022-10-26 16:12:42 +0900

    blog.index 뷰에 Post 조회추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    # 전체 포스팅을 가지고 올 준비 아직 안 가지고옴
+    post_qs = Post.objects.all().order_by('-id')
+    return render(request, 'blog/index.html', {
+        'post_list' : post_qs, } )
