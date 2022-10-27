a1304f65723f3f68ac2e256dd0689552e9421c58
yczero <yc4038@naver.com>
2022-10-27 11:52:28 +0900

    20221027오전 수업

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,14 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def get_absolute_url(self):
+
+        # 향후에는 장고의 url reverse 기능을 활용하기
+        return f"/blog/{ self.pk }/"
+
     def __str__(self):
         # self.pk # int 숫자필드?
         return f'[{self.pk}] {self.title}'
+   
+
    
